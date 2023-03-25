8e14613921c1fafea42743d44c4f623a43cbb6ab
git <gd1064@wildcats.unh.edu>
2023-03-25 06:05:32 -0400

    commiting before generating mod files

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -15,17 +15,39 @@
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
+pub trait Grade {
+    fn print(&self) -> String;
+}
+
+struct NumericGrade {
+    grade: f32,
+}
+struct AlphabeticGrade {
+    grade: String,
+}
+impl Grade for NumericGrade {
+    fn print(&self) -> String {
+        format!("{}", self.grade)
+    }
+}
+impl Grade for AlphabeticGrade {
+    fn print(&self) -> String {
+        format!("{}", self.grade)
+    }
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -50,7 +72,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
---
+++ some_dudes_script.py
@@ -0,0 +1,40 @@
+# this script will generate mod.rs files that let rust-analyzer work
+# without `rust-project.json` on the rustlings examples.
+
+import os
+import glob
+
+for folder in glob.glob("exercises/*"):
+    if not os.path.isdir(folder):
+        continue
+    with open(os.path.join(folder, "mod.rs"), "w") as f:
+        for file in glob.glob(os.path.join(folder, "*.rs")):
+            if not os.path.isfile(file) or os.path.basename(file) == "mod.rs":
+                continue
+            filename = os.path.splitext(os.path.basename(file))[0]
+            f.write(f"mod r#{filename};\n")
+
+# write the base module
+with open("exercises/mod.rs", "w") as f:
+    f.write("#![allow(dead_code)]\n")
+    for folder in glob.glob("exercises/*"):
+        if os.path.isdir(folder):
+            foldername = os.path.basename(folder)
+            f.write(f"mod r#{foldername};\n")
+    # for the root files (like quiz1.rs)
+    for rust_file in glob.glob("exercises/*.rs"):
+        if os.path.isfile(rust_file) and os.path.basename(rust_file) != "mod.rs":
+            filename = os.path.splitext(os.path.basename(rust_file))[0]
+            f.write(f"mod r#{filename};\n")
+
+# make sure cargo.toml has the lib defined:
+lib = """
+[lib]
+name = "exercises"
+path = "exercises/mod.rs"
+"""
+
+with open("Cargo.toml", "r") as f:
+    if 'name = "exercises"' not in f.read():
+        with open("Cargo.toml", "a") as f:
+            f.write(lib)
